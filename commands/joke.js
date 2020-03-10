83d52738142008a34ed7ec5381a3be1c23b30757
Basheer <59171316+Basheer3648734@users.noreply.github.com>
2020-03-10 15:37:47 +0530

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -3,7 +3,7 @@ const request = require('request')
 
 module.exports = {
 	name: 'joke',
-	discription: 'returns a joke',
+	description: 'returns a joke',
 	execute(message, args) {
 		const url = 'https://official-joke-api.appspot.com/random_joke';
 		request.get(url, (error, response, body) => {
